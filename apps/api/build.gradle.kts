4580ab4b836f97fd70ea82bc69a843ec96cd07a5
Manuel Lorenzo <manulorenzop@gmail.com>
2026-02-22 22:35:49 +0100

    add spring boot starter jpa dependency

--- apps/api/build.gradle.kts
+++ apps/api/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.jpa)
 }
 
 group = "me.manulorenzo"
@@ -14,11 +15,16 @@ dependencies {
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.validation)
     implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.jackson.module.kotlin)
     implementation(libs.kotlin.reflect)
 
     testImplementation(libs.spring.boot.starter.test)
     testImplementation("com.tngtech.archunit:archunit-junit4:1.4.1")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
